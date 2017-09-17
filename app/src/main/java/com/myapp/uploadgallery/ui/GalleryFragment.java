44dcad8e9b841c71c3f5663c8eec55ff1d42ed63
Margarita Litkevych <margarita.litkevych@globallogic.com>
2017-09-17 21:10:27 +0300

    added layout for the gallery fragment

--- app/src/main/java/com/myapp/uploadgallery/ui/GalleryFragment.java
+++ app/src/main/java/com/myapp/uploadgallery/ui/GalleryFragment.java
@@ -1,10 +1,35 @@
 package com.myapp.uploadgallery.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.myapp.uploadgallery.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Has a gallery view with already uploaded images.
  */
 public class GalleryFragment extends Fragment {
+    @BindView(R.id.rvGallery)
+    RecyclerView galleryView;
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_gallery, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable final Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 }
