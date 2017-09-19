d66c9b4fe92d248f3e570426fb9c247d1065f653
Margarita Litkevych <margarita.litkevych@globallogic.com>
2017-09-19 23:40:42 +0300

    Added test

--- app/src/androidTest/java/com/myapp/uploadgallery/di/MockAppComponent.java
+++
@@ -1,7 +0,0 @@
-package com.myapp.uploadgallery.di;
-
-//@Singleton
-//@Component(modules = {MockAppModule.class})
-//public interface MockAppComponent extends AppComponent {
-//    void inject(MainActivityTest mainActivityTest);
-//}
