4c71ad0fb61f7a0006e45649ded9ea1e5b748f52
Margarita Litkevych <margarita.litkevych@globallogic.com>
2017-09-27 13:51:31 +0300

    Refactor

--- app/src/androidTest/java/com/myapp/uploadgallery/api/TestGalleryEndpoint.java
+++ app/src/androidTest/java/com/myapp/uploadgallery/api/TestGalleryEndpoint.java
@@ -1,5 +1,9 @@
 package com.myapp.uploadgallery.api;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
+
 import io.reactivex.Single;
 import okhttp3.MultipartBody;
 import retrofit2.http.Part;
@@ -7,6 +11,8 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public class TestGalleryEndpoint implements GalleryEndpoint {
+    private int imageId = 0;
+    private DateTimeFormatter formatter = ISODateTimeFormat.dateTimeNoMillis();
     @Override
     public Single<ImageResponse> getImagesForUser(@Query("user_id") final String userId) {
         final GalleryImage img1 = new GalleryImage("http://placehold.it/120x120&text=image1",
@@ -36,22 +42,9 @@ public class TestGalleryEndpoint implements GalleryEndpoint {
     @Override
     public Single<GalleryImage> postImageForUser(@Path("userId") final String userId,
                                                  @Part final MultipartBody.Part image) {
-        final GalleryImage img1 = new GalleryImage("http://placehold.it/120x120&text=image1",
-                "2017-09-18T15:14:20Z");
-        final GalleryImage img2 = new GalleryImage("http://placehold.it/120x120&text=image2",
-                "2017-09-19T15:30:27Z");
-        final GalleryImage img3 = new GalleryImage("http://placehold.it/120x120&text=image3",
-                "2017-09-20T16:20:37Z");
-        switch (userId) {
-            case "user1": {
-                return Single.just(img1);
-            }
-            case "user2": {
-                return Single.just(img2);
-            }
-            default: {
-                return Single.just(img3);
-            }
-        }
+        DateTime dateTime = new DateTime();
+        GalleryImage img = new GalleryImage("http://placehold.it/120x120&text=image" + (++imageId),
+                dateTime.toString(formatter));
+        return Single.just(img);
     }
 }
